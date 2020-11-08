eac6296d8de7027e2c0e6806756fd2209dbac7bc
Miguel Galvao <miguelgalvao@live.com.pt>
2020-11-08 21:42:08 +0000

    cleaned repo for 2nd srint

--- compile.sh
+++ compile.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-
-# run lex and compile the resulting C analyser
-lex $1
-clang -o $2 lex.yy.c
-
-# 'lex' and 'gcc' are commonly available too
+lex uccompiler.l && yacc -d uccompiler.y && cc -o uccompiler lex.yy.c y.tab.c
