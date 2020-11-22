13c0f1d1aee2c6cb20dbe86cf567455404e4efe3
joaobndomingues <joaobndomingues@gmail.com>
2020-11-22 22:46:49 +0000

    functions to create symbol tables done

--- symbol_table.h
+++ symbol_table.h
@@ -1,10 +1,16 @@
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
 
+typedef struct _t4 {
+    char * id;
+    char * type;        //int, double ou char
+    struct _t4 * next;
+} func_parameter;
+
 typedef struct _t3 {
     char * id;
     char * type;        //int, double ou char
-    char * parameters;  //"int, int, int"
+    func_parameter * parameters;  //"int, int, int"
 } func_declaration;
 
 typedef struct _t2 {
@@ -35,5 +41,12 @@ typedef struct _t0 {
 
     struct _t0 * next;
 } table;
+
+
+table_element *insert_function(table *tab, char * id, char * type);
+table_element *insert_variable(table *tab, char * id, char * type, char * param);
+table *insert_table(char * name);
+var_declaration *search_declaration(char * id, table * tab);
+func_declaration *search_function(char * id, table * tab);
 
 #endif
