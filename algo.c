a4ef28f1c2285f34b476f9a37ba70a5287a4ca2c
Miguel Galvao <miguelgalvao@live.com.pt>
2020-12-20 10:45:45 +0000

    384 points

--- algo.c
+++ algo.c
@@ -1,4 +1,29 @@
-int main(void)
+void whileAndIfChains(void)
 {
-    return 0;
+
+    //prints: ABC
+
+    int a = 5;
+    int b;
+    while (a <= 10)
+    {
+        b = 1;
+        if (b == 2)
+        {
+        }
+        else
+        {
+
+            if (a == 7)
+            {
+                putchar('\n');
+                putchar('A');
+                putchar('B');
+                putchar('C');
+                putchar('\n');
+            }
+        }
+
+        a = a + 1;
+    }
 }
